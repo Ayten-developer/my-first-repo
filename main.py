f7d104919aafc3dc6592f4662e5783e99cd1d8fc
Ayten-developer <ayten.tookie@gmail.com>
2024-07-16 04:09:04 +0300

    edittig browser with dev

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 print("successfully pushed first trial on github")
+<<<<<<< HEAD
+print("editting on browser")
+=======
 print("editting no vscode")
 print("second pull no vscode")
 print("third pull request")
+
+>>>>>>> 5cbaa2dc9bf898fcaddfff8136fafcc96104c947
